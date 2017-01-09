358b5f899116f521aafc44c6d6fe4c9ae63374ed
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-01-09 21:11:28 -0200

    Cut off usernames > 32, print to the console when response != empty

--- src/com/mrpowergamerbr/temmiewebhook/DiscordMessage.java
+++ src/com/mrpowergamerbr/temmiewebhook/DiscordMessage.java
@@ -10,13 +10,13 @@ public class DiscordMessage {
 	}
 	
 	public DiscordMessage(String username, String content, String avatar_url) {
-		this.username = username;
-		this.content = content;
-		this.avatar_url = avatar_url;
+		setUsername(username);
+		setContent(content);
+		setAvatarUrl(avatar_url);
 	}
 	
 	public void setUsername(String username) {
-		this.username = username;
+		this.username = username.substring(0, Math.min(32, username.length()));
 	}
 	
 	public void setContent(String content) {
--- src/com/mrpowergamerbr/temmiewebhook/TemmieWebhook.java
+++ src/com/mrpowergamerbr/temmiewebhook/TemmieWebhook.java
@@ -6,20 +6,24 @@ import com.google.gson.Gson;
 public class TemmieWebhook {
 	public static final Gson gson = new Gson();
 	public String url;
-	
+
 	public TemmieWebhook(String url) {
 		this.url = url;
 	}
-	
+
 	public void sendMessage(DiscordMessage dm) {
-		HttpRequest.post("https://discordapp.com/api/webhooks/266185886104813568/KJCT1OurPHemam39Q8WZx1NbHxFJ0NFlhpITg5LLTRQ1-LB-oGMIWDUDQbEFXP_NR5qS")
+		String response = HttpRequest.post("https://discordapp.com/api/webhooks/266185886104813568/KJCT1OurPHemam39Q8WZx1NbHxFJ0NFlhpITg5LLTRQ1-LB-oGMIWDUDQbEFXP_NR5qS")
 				.acceptJson()
 				.contentType("application/json")
 				.header("User-Agent", "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11") // Why? Because discordapp.com blocks the default User Agent
 				.send(gson.toJson(dm))
 				.body();
+
+		if (!response.isEmpty()) {
+			System.out.println(response);
+		}
 	}
-	
+
 	public void sendEmbed() {
 		// soon
 	}
